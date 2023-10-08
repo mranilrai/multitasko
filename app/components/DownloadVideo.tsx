320cc17de275bc1fe6dd18ec84e558681a933d81
Anil Kumar Rai <anilrai@outlook.com>
2023-10-08 22:19:34 +0530

    fixing download issue

--- app/components/DownloadVideo.tsx
+++ app/components/DownloadVideo.tsx
@@ -7,35 +7,36 @@ import { youtubeVideoDownloadAction } from "../actions";
 import { SubmitButton } from "./common/SubmitButton";
 
 interface DownloadVideoProps {
-  format: {
-    itag: number;
-  };
-  videoLink: string;
-  title: string
+    format: {
+        itag: number;
+    };
+    videoLink: string;
+    title: string
 }
 export default function DownloadVideo({
-  format,
-  videoLink,
-  title
+    format,
+    videoLink,
+    title
 }: DownloadVideoProps) {
-  const [state, formAction] = useFormState(youtubeVideoDownloadAction, {});
+    const [state, formAction] = useFormState(youtubeVideoDownloadAction, {});
 
-  useEffect(() => {
-    if (state.videoDownloadPath) {
-      const link = document.createElement("a");
-      link.href = state.videoDownloadPath;
-      link.download = title;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  }, [state.videoDownloadPath]);
+    useEffect(() => {
+        if (state.videoDownloadPath) {
+            console.log(state.videoDownloadPath);
+            const link = document.createElement("a");
+            link.href = state.videoDownloadPath;
+            link.download = title;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }, [title, state.videoDownloadPath]);
 
-  return (
-    <form action={formAction}>
-      <input type="text" name="itag" hidden value={format.itag} readOnly />
-      <input type="text" name="videoLink" hidden value={videoLink} readOnly />
-      <SubmitButton className="px-2 bg-red-600" text="Download" />
-    </form>
-  );
+    return (
+        <form action={formAction}>
+            <input type="text" name="itag" hidden value={format.itag} readOnly />
+            <input type="text" name="videoLink" hidden value={videoLink} readOnly />
+            <SubmitButton className="px-2 bg-red-600" text="Download" />
+        </form>
+    );
 }
